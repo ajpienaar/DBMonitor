35d3d9dcba1b338b446a44838a92d2ec63d15afa
ajpienaar <ajpienaar123@gmail.com>
2023-10-20 11:42:50 +1300

    DB config file

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// Edit MySQL credentials here
+    define( "DBHOST", "");
+    define( "DBUSER", "");
+    define( "DBPASSWORD", "");
+    define( "DBDATABASE", ""); // Add SQL details here
+?>
